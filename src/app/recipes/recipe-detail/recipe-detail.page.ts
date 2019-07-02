48dd5c01df6a5cc9cb7c1b01d95834f84448e73d
Sergio Luis Medina Hernández <sermed19@gmail.com>
2019-07-02 16:55:19 -0400

    uso de prettier

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {RecipesService} from '../recipes.service';
-import {Recipe} from '../recipes.model';
-import {AlertController} from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { RecipesService } from "../recipes.service";
+import { Recipe } from "../recipes.model";
+import { AlertController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.page.html',
-  styleUrls: ['./recipe-detail.page.scss'],
+  selector: "app-recipe-detail",
+  templateUrl: "./recipe-detail.page.html",
+  styleUrls: ["./recipe-detail.page.scss"]
 })
 export class RecipeDetailPage implements OnInit {
-
   loadedRecipe: Recipe;
 
   constructor(
@@ -18,28 +17,38 @@ export class RecipeDetailPage implements OnInit {
     private recipesService: RecipesService,
     private router: Router,
     private alertCtrl: AlertController
-    ) { }
+  ) {}
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(paramMap => {
-      if (!paramMap.has('recipeId')){
-        this.router.navigate(['/recipes']);
+      if (!paramMap.has("recipeId")) {
+        this.router.navigate(["/recipes"]);
 
         return;
       }
-      const recipeId = paramMap.get('recipeId');
+      const recipeId = paramMap.get("recipeId");
       this.loadedRecipe = this.recipesService.getRecipe(recipeId);
     });
   }
 
   onDeleteRecipe() {
-    this.alertCtrl.create({header: 'Cuidado', message: 'Desea eliminar este elemento?', buttons: [
-      {text: 'okay', handler: () => {
-        this.recipesService.deleteRecipe(this.loadedRecipe.id);
-        this.router.navigate(['/recipes']);
-      } },
-      {text: 'Cancel', role: 'cancel'}]
-    }).then(alertEl => {alertEl.present(); });
+    this.alertCtrl
+      .create({
+        header: "Cuidado",
+        message: "Desea eliminar este elemento?",
+        buttons: [
+          {
+            text: "okay",
+            handler: () => {
+              this.recipesService.deleteRecipe(this.loadedRecipe.id);
+              this.router.navigate(["/recipes"]);
+            }
+          },
+          { text: "Cancel", role: "cancel" }
+        ]
+      })
+      .then(alertEl => {
+        alertEl.present();
+      });
   }
-
 }
